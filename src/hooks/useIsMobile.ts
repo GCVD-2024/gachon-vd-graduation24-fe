9ee2f02a03fe197a77e61764d118fc23eefdb34f
anonymousrecords <a.Records2022@gmail.com>
2024-08-06 18:06:59 +0900

    chore: useIsMobile 훅 구현

---
+++ src/hooks/useIsMobile.ts
@@ -0,0 +1,20 @@
+  /* TO DO : 임시 설정 */
+import { useState, useEffect } from 'react';
+import { MOBILE_BREAKPOINT } from '../constants/constants';
+
+export const useIsMobile = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkIsMobile = () => {
+      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
+    };
+
+    checkIsMobile();
+    window.addEventListener('resize', checkIsMobile);
+
+    return () => window.removeEventListener('resize', checkIsMobile);
+  }, []);
+
+  return isMobile;
+};

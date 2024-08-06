c656d2bc735e9bb8fab84fab4fc8b07c05af2a4c
anonymousrecords <a.Records2022@gmail.com>
2024-08-06 18:06:44 +0900

    chore: constants 파일 생성

---
+++ src/constants/constants.ts
@@ -0,0 +1,7 @@
+export const NAVIGATION_ITEMS = [
+  { name: 'ABOUT', path: '/' },
+  { name: 'WORKS', path: '/work' },
+  { name: 'GUEST', path: '/guest' },
+];
+
+export const MOBILE_BREAKPOINT = 768;

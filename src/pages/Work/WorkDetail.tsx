05fbf5f371de3bebf0f2ec02a088cd91ce7c8a37
urjimyu <92876819+urjimyu@users.noreply.github.com>
2024-09-15 18:06:31 +0900

    feat: youtube 플레이어 추가

--- src/pages/Work/WorkDetail.tsx
+++ src/pages/Work/WorkDetail.tsx
@@ -1,10 +1,23 @@
 import styled from 'styled-components';
 import WorkInfoSection from './components/WorkInfoSection';
+import YouTube from 'react-youtube';
 
 const WorkDetail = () => {
   return (
     <WorkDetailPage>
       <WorkInfoSection />
+      <YouTube
+        // videoId={key}
+        opts={{
+          width: '1220',
+          height: '686',
+          playerVars: {
+            autoplay: 1,
+            rel: 0,
+            modestbranding: 1,
+          },
+        }}
+      />
       <WorkImg src="" alt="작품-이미지" />
     </WorkDetailPage>
   );

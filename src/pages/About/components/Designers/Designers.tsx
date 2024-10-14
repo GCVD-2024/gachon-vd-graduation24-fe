b6412f2f22ec8e0055a41e63d9b88198b2d4854b
anonymousrecords <a.Records2022@gmail.com>
2024-10-14 22:28:49 +0900

    chore: update designers list

--- src/pages/About/components/Designers/Designers.tsx
+++ src/pages/About/components/Designers/Designers.tsx
@@ -3,92 +3,81 @@ import styled from 'styled-components';
 import { useIsMobile } from '../../../../hooks/useIsMobile';
 
 const designers = [
-  '김가현',
-  '김가현',
-  '김가현',
-  '조예진',
-  '김가현',
-  '김가현',
-  '김가현',
-  '김가현',
-  '김가현',
-  '김가현',
-  '김가현',
-  '김가현',
-  '김가현',
-  '김가현',
+  '이다현',
+  '권현아',
+  '안준범',
+  '송영민',
+  '정시온',
+  '박초희',
+  '김채은',
+  '류현주',
+  '이유정',
+  '허윤정',
+  '오태현',
+  '강지은',
+  '이형철',
+  '정지우',
+  '김지수',
+  '노지은',
+  '이은재',
+  '유서연',
+  '안우현',
+  '신하은',
   '홍정우',
-  '김가현',
-  '김가현',
-  '김가현',
-  '김가현',
+  '김지용',
+  '최홍혁',
+  '정다현',
+  '박주성',
+  '이가연',
+  '백승빈',
+  '이지윤',
+  '김예현',
+  '신도한',
+  '박수지',
+  '김지우',
+  '김지원',
+  '심지현',
+  '조원형',
+  '강민재',
+  '장은영',
+  '유지민',
+  '황주현',
+  '정윤선',
+  '장채원',
+  '김민지',
+  '류정경',
+  '김용일',
   '나선주',
-  '김가현',
-  '김가현',
-  '김가현',
+  '김민지',
+  '이가영',
+  '박연수',
+  '김도연',
+  '박서연',
+  '조예진',
   '정다연',
-  '김가현',
-  '김가현',
-  '김가현',
-  '김가현',
-  '김가현',
-  '김가현',
-  '김가현',
-  '김가현',
-  '김가현',
-  '김가현',
-  '김가현',
-  '이경택',
-  '김가현',
   '이한별',
-  '김가현',
-  '김가현',
-  '김가현',
-  '김가현',
-  '심한별',
-  '김가현',
-  '김가현',
-  '김가현',
-  '김가현',
-  '김가현',
-  '김가현',
-  '김가현',
-  '김가현',
-  '김가현',
-  '김가현',
-  '김가현',
-  '김가현',
-  '김가현',
-  '김도연',
-  '김가현',
-  '김가현',
-  '김가현',
-  '김가현',
-  '김가현',
-  '김가현',
-  '김가현',
-  '김가현',
-  '김가현',
-  '김도연',
-  '김가현',
-  '김가현',
-  '김가현',
+  '이경택',
+  '오민택',
+  '박이레',
+  '조예찬',
+  '이지민',
+  '오치영',
+  '채승우'
 ];
-
 export default function Designers() {
   const containerRef = useRef<HTMLDivElement>(null);
   const isMobile = useIsMobile();
 
   return (
     <DesignersWrapper ref={containerRef} isMobile={isMobile}>
-        <Title isMobile={isMobile}>DESIGNERS</Title>
-        <DesignersGrid isMobile={isMobile}>
-          {designers.map((designer, index) => (
-            <DesignerName key={index} isMobile={isMobile}>
-              {designer}
-            </DesignerName>
-          ))}
-        </DesignersGrid>
+      <Title isMobile={isMobile}>DESIGNERS</Title>
+      <DesignersGrid isMobile={isMobile}>
+        {designers.map((designer, index) => (
+          <DesignerName key={index} isMobile={isMobile}>
+            {designer}
+          </DesignerName>
+        ))}
+      </DesignersGrid>
     </DesignersWrapper>
   );
 }

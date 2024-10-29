d34d388ecb833291227b4ff72a1c2372c3431ecc
anonymousrecords <a.Records2022@gmail.com>
2024-10-29 23:43:39 +0900

    style: apply #121212 to GuestPage

--- src/pages/Guest/Guest.tsx
+++ src/pages/Guest/Guest.tsx
@@ -222,7 +222,7 @@ const Title = styled.h1`
 const GuestPage = styled.main`
   width: 100vw;
   height: calc(100vh - 73px);
-  background: radial-gradient(41.45% 43.19% at 50% 50%, #00b4db 0%, #000000 100%);
+  background: radial-gradient(41.45% 43.19% at 50% 50%, #00b4db 0%, #121212 100%);
   overflow-x: auto;
   display: flex;
   flex-direction: column;
